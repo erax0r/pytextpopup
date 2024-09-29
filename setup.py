79a492080704f0b9574de3e49bf0e40b488b17fd
erax0r <erax0r@gmail.com>
2024-09-29 13:02:02 -0700

    cleanup structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="pytextpopup",
-    version="0.1.0",
+    version="0.2.2",
     author="Andrew Goetz",
     author_email="erax0r@gmail.com",
     description="A Python package that displays floating text windows on the screen at the cursor's position. The text can be customized with different fonts, colors, and scroll speeds.",
